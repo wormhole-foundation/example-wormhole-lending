c8dc4d7eef3221fc7eb448062bbda16a6044e91b
Karl Kempe <karlkempe@users.noreply.github.com>
2022-09-29 05:40:00 +0000

    evm: add struct for future use

--- evm/src/CrossChainBorrowLendStructs.sol
+++ evm/src/CrossChainBorrowLendStructs.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+struct DepositedBorrowedUints {
+    uint256 deposited;
+    uint256 borrowed;
+}
+
 struct NormalizedTotalAmounts {
     uint256 deposited;
     uint256 borrowed;
