7056316f15aa6a34d776815ffe1c9c88bfa051e3
Reptile <43194093+gator-boi@users.noreply.github.com>
2022-10-05 17:25:12 -0500

    Set up contracts for second example (#8)
    Co-authored-by: gator-boi <gator-boi@users.noreply.github.com>

---
+++ example-2/evm/src/contracts/lendingHub/HubSetters.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./HubState.sol";
+
+contract HubSetters is HubState {
+    function setOwner(address owner) internal {
+        _state.owner = owner;
+    }
+
+    function setChainId(uint16 chainId) internal {
+        _state.provider.chainId = chainId;
+    }
+
+    function setWormhole(address wormholeAddress) internal {
+        _state.provider.wormhole = payable(wormholeAddress);
+    }
+
+    function setTokenBridge(address tokenBridgeAddress) internal {
+        _state.provider.tokenBridge = tokenBridgeAddress;
+    }
+}
---
+++ example-2/evm/src/contracts/lendingSpoke/SpokeMessages.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../../libraries/external/BytesLib.sol";
+
+import "./SpokeStructs.sol";
+
+contract SpokeMessages {
+    using BytesLib for bytes;
+}
