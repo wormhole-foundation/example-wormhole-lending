4f962f8f248471eb381aa095dff15d5dbd7f2946
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2022-10-07 08:49:57 -0500

    Hub messages (#12)
    * Update HubMessages.sol
    
    * Update HubStructs.sol

--- example-2/evm/src/contracts/lendingHub/HubMessages.sol
+++ example-2/evm/src/contracts/lendingHub/HubMessages.sol
@@ -7,4 +7,134 @@ import "./HubStructs.sol";
 
 contract HubMessages {
     using BytesLib for bytes;
+
+    function encodeMessageHeader(HubStructs.MessageHeader memory header)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                header.sender
+            );
+    }
+
+    function encodeDepositMessage(HubStructs.DepositMessage memory message)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                uint8(1), // payloadID
+                encodeMessageHeader(message.header),
+                message.assetAddresses,
+                message.assetAmounts
+            );
+    }
+
+    function encodeWithdrawMessage(HubStructs.WithdrawMessage memory message)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                uint8(2), // payloadID
+                encodeMessageHeader(message.header),
+                message.assetAddresses.length,
+                message.assetAddresses,
+                message.assetAmounts
+            );
+    }
+
+    function encodeBorrowMessage(HubStructs.BorrowMessage memory message)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                uint8(3), // payloadID
+                encodeMessageHeader(message.header),
+                message.assetAddresses.length,
+                message.assetAddresses,
+                message.assetAmounts
+            );
+    }
+
+    function encodeRepayMessage(HubStructs.RepayMessage memory message)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                uint8(4), // payloadID
+                encodeMessageHeader(message.header),
+                message.assetAddresses.length,
+                message.assetAddresses,
+                message.assetAmounts
+            );
+    }
+
+    function encodeLiquidationMessage(HubStructs.LiquidationMessage memory message)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return
+            abi.encodePacked(
+                uint8(5), // payloadID
+                encodeMessageHeader(message.header),
+                message.vault,
+                message.assetRepayAddresses.length,
+                message.assetReceiptAddresses.length,
+                message.assetRepayAddresses,
+                message.assetRepayAmounts,
+                message.assetReceiptAddresses,
+                message.assetReceiptAmounts
+            );
+    }
+
+    function decodeMessageHeader(bytes memory serialized)
+        internal
+        pure
+        returns (HubStructs.MessageHeader memory header)
+    {
+        uint256 index = 0;
+
+        // parse the header
+        header.payloadID = serialized.toUint8(index += 1);
+        header.sender = serialized.toAddress(index += 20);
+    }
+
+    function decodeDepositMessage(bytes memory serialized)
+        internal
+        pure
+        returns (HubStructs.DepositMessage memory params)
+    {
+        uint256 index = 0;
+
+        // parse the message header
+        params.header = decodeMessageHeader(
+            serialized.slice(index, index += 21)
+        );
+        uint32 length = serialized.toUint32(index);
+        index += 4;
+
+        address[] memory assetAddresses = new address[](length);
+        /*
+        params.borrowAmount = serialized.toUint256(index += 32);
+        params.totalNormalizedBorrowAmount = serialized.toUint256(index += 32);
+        params.interestAccrualIndex = serialized.toUint256(index += 32);
+
+        require(params.header.payloadID == 1, "invalid message");
+        require(index == serialized.length, "index != serialized.length");
+        require(assetAddresses.length == length, "Asset addresses length is incorrect");
+        */
+
+    }
+
+
 }
