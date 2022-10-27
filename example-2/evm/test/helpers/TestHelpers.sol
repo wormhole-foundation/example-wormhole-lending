908510c134c92e97722e79de27a30a26fb422736
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2022-10-27 13:27:35 -0400

    Testing prank mint (#32)
    * Testing prank mint
    
    * Remove changes to MyERC20
    
    * fixed fake minting
    
    * Changed hardcoded addresses to refer to asset[0] and asset[1], and also changed testFail to use vm.expectRevert
    
    Co-authored-by: wrinkledLatherer

--- example-2/evm/test/helpers/TestHelpers.sol
+++ example-2/evm/test/helpers/TestHelpers.sol
@@ -14,6 +14,8 @@ import {HubUtilities} from "../../src/contracts/lendingHub/HubUtilities.sol";
 import {MyERC20} from "./MyERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
 import {IWormhole} from "../../src/interfaces/IWormhole.sol";
 import {ITokenBridge} from "../../src/interfaces/ITokenBridge.sol";
 import {ITokenImplementation} from "../../src/interfaces/ITokenImplementation.sol";
@@ -30,6 +32,7 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
         IERC20 asset;
         uint256 collateralizationRatioDeposit;
         uint256 collateralizationRatioBorrow;
+        
         uint8 decimals;
         uint256 reserveFactor;
         bytes32 pythId;
@@ -243,8 +246,12 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
         );
     }
 
+    function doDeposit(address vault, TestAsset memory asset, uint256 assetAmount) internal returns (bytes memory encodedVM) {
+        doDeposit(vault, asset, assetAmount, false, "");
+    }
+
     // create Deposit payload and package it into TokenBridgePayload into WH message and send the deposit
-    function doDeposit(address vault, TestAsset memory asset, uint256 assetAmount)
+    function doDeposit(address vault, TestAsset memory asset, uint256 assetAmount, bool expectRevert, string memory revertString)
         internal
         returns (bytes memory encodedVM)
     {
@@ -276,16 +283,23 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
         encodedVM = getSignedWHMsgTransferTokenBridge(transfer);
 
         // complete deposit
+        if(expectRevert) {
+            wormholeData.vm.expectRevert(bytes(revertString));
+        }
         wormholeData.hub.completeDeposit(encodedVM);
     }
 
+    function doRepay(address vault, TestAsset memory asset, uint256 assetAmount) internal returns (bytes memory encodedVM) {
+        doRepay(vault, asset, assetAmount, false, "");
+    }
+
     // create Deposit payload and package it into TokenBridgePayload into WH message and send the deposit
-    function doRepay(address vault, TestAsset memory asset, uint256 assetAmount)
+    function doRepay(address vault, TestAsset memory asset, uint256 assetAmount, bool expectRevert, string memory revertString)
         internal
         returns (bytes memory encodedVM)
     {
         address assetAddress = asset.assetAddress;
-        // create Deposit payload
+        // create Repay payload
         PayloadHeader memory header = PayloadHeader({
             payloadID: uint8(4),
             sender: vault //address(uint160(uint(keccak256(abi.encodePacked(block.timestamp)))))
@@ -312,12 +326,19 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
 
         encodedVM = getSignedWHMsgTransferTokenBridge(transfer);
 
-        // complete deposit
+        // complete repay
+        if(expectRevert) {
+            wormholeData.vm.expectRevert(bytes(revertString));
+        }
         wormholeData.hub.completeRepay(encodedVM);
     }
 
+    function doBorrow(address vault, TestAsset memory asset, uint256 assetAmount) internal returns (bytes memory encodedVM) {
+        doBorrow(vault, asset, assetAmount, false, "");
+    }
+
     // create Borrow payload and package it into TokenBridgePayload into WH message and send the borrow
-    function doBorrow(address vault, TestAsset memory asset, uint256 assetAmount)
+    function doBorrow(address vault, TestAsset memory asset, uint256 assetAmount, bool expectRevert, string memory revertString)
         internal
         returns (bytes memory encodedVM)
     {
@@ -349,11 +370,18 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
         encodedVM = getSignedWHMsgCoreBridge(serialized);
 
         // complete borrow
+         if(expectRevert) {
+            wormholeData.vm.expectRevert(bytes(revertString));
+        }
         wormholeData.hub.completeBorrow(encodedVM);
     }
 
+    function doWithdraw(address vault, TestAsset memory asset, uint256 assetAmount) internal returns (bytes memory encodedVM) {
+        doWithdraw(vault, asset, assetAmount, false, "");
+    }
+
     // create Withdraw payload and package it into TokenBridgePayload into WH message and send the withdraw
-    function doWithdraw(address vault, TestAsset memory asset, uint256 assetAmount)
+    function doWithdraw(address vault, TestAsset memory asset, uint256 assetAmount, bool expectRevert, string memory revertString)
         internal
         returns (bytes memory encodedVM)
     {
@@ -367,6 +395,9 @@ contract TestHelpers is HubStructs, HubMessages, HubGetters, HubUtilities {
         encodedVM = getSignedWHMsgCoreBridge(serialized);
 
         // complete withdraw
+        if(expectRevert) {
+            wormholeData.vm.expectRevert(bytes(revertString));
+        }
         wormholeData.hub.completeWithdraw(encodedVM);
     }
 
